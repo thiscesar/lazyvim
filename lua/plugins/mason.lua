6caf161d4f57a05f748910380dcb378f29247911
brunojoao <bruno.garcia@essentia.com.br>
2024-09-25 10:50:40 -0300

    add blade laravel support

---
+++ lua/plugins/mason.lua
@@ -0,0 +1,14 @@
+return {
+    "williamboman/mason.nvim",
+    opts = {
+        ensure_installed = {
+            "blade-formatter",
+            "docker-compose-language-service",
+            "dockerfile-language-server",
+            "intelephense",
+            "phpactor",
+            "pint",
+            "prettierd"
+        },
+    },
+}
