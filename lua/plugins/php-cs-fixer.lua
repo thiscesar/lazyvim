6b9e3aff762d5de5b5cd11cd0f4b212a27c7be83
brunojoao <bruno.garcia@essentia.com.br>
2024-10-28 13:47:26 -0300

    add php-cs-fixer

---
+++ lua/plugins/php-cs-fixer.lua
@@ -0,0 +1,36 @@
+return {
+  "stephpy/vim-php-cs-fixer",
+  lazy = false,
+  config = function()
+    -- Mapeia o comando para corrigir o diretório inteiro
+    vim.api.nvim_set_keymap("n", "<leader>Pcd", ":call PhpCsFixerFixDirectory()<CR>", { noremap = true, silent = true })
+
+    -- Mapeia o comando para corrigir o arquivo atual
+    vim.api.nvim_set_keymap("n", "<leader>Pcf", ":call PhpCsFixerFixFile()<CR>", { noremap = true, silent = true })
+
+    -- Configurações do php-cs-fixer
+    -- Se php-cs-fixer estiver no $PATH, você não precisa definir a linha abaixo
+    -- vim.g.php_cs_fixer_path = "~/php-cs-fixer.phar"  -- define o caminho para o php-cs-fixer.phar
+
+    -- Se você usar a versão 1.x do php-cs-fixer
+    vim.g.php_cs_fixer_level = "symfony"               -- opções: --level (default:symfony)
+    vim.g.php_cs_fixer_config = "default"              -- opções: --config
+    -- Se você quiser definir fixadores específicos:
+    -- vim.g.php_cs_fixer_fixers_list = "linefeed,short_tag" -- opções: --fixers
+    -- vim.g.php_cs_fixer_config_file = '.php_cs'      -- opções: --config-file
+    -- Fim dos parâmetros de configuração da versão 1 do php-cs-fixer
+
+    -- Se você usar a versão 2.x do php-cs-fixer
+    vim.g.php_cs_fixer_rules = "@PSR2"                 -- opções: --rules (default:@PSR2)
+    -- vim.g.php_cs_fixer_cache = ".php_cs.cache"      -- opções: --cache-file
+    -- vim.g.php_cs_fixer_config_file = '.php_cs'      -- opções: --config
+    vim.g.php_cs_fixer_allow_risky = "yes"             -- opções: --allow-risky
+    -- Fim dos parâmetros de configuração da versão 2 do php-cs-fixer
+
+    vim.g.php_cs_fixer_php_path = "php"                -- Caminho para o PHP
+    vim.g.php_cs_fixer_enable_default_mapping = 0      -- Ativa o mapeamento por padrão (<leader>pcd)
+    vim.g.php_cs_fixer_dry_run = 0                      -- Chama o comando com a opção dry-run
+    vim.g.php_cs_fixer_verbose = 0                      -- Retorna a saída do comando se 1, caso contrário, uma informação inline.
+    -- vim.g.php_cs_fixer_ignore_env = 1                 -- Ignorar quaisquer requisitos de ambiente
+  end,
+}
