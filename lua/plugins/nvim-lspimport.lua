5ea9a293805588c37f641d4b90619a9422814264
brunojoao <bruno.garcia@essentia.com.br>
2024-10-31 14:02:29 -0300

    add code action import 2

--- lua/plugins/nvim-lspimport.lua
+++ lua/plugins/nvim-lspimport.lua
@@ -2,7 +2,7 @@ return {
 	"stevanmilic/nvim-lspimport",
 	lazy = false,
     config = function()
-        local lspimport = require("lspimport").setup()
+        local lspimport = require("lspimport")
 		vim.keymap.set("n", "<leader>a", lspimport.import, { noremap = true })
     end,
 }
