3a18bfbb5501b11fd6ea7ce1c2d507bce1ee800a
brunojoao <bruno.garcia@essentia.com.br>
2024-11-01 19:16:12 -0300

    telescope add ignore list

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,12 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+require('telescope').setup{
+  defaults = {
+    file_ignore_patterns = {
+      "public/assets/plugins/*",
+      "vendor/*",
+      "node_modules/*",
+    },
+  },
+}
